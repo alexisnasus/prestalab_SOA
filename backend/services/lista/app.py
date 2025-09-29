75f8fe55a74370fd5f075bcb6f684e267adba69d
Rafael <62951054+Rderokha@users.noreply.github.com>
2025-09-29 13:13:56 -0300

    feat: add service components to backend directory
    create services entrypoints + Dockerfile for each one
    create docker-compose.yml

---
+++ backend/services/lista/app.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, HTTPException
+from sqlalchemy import create_engine, text
+import os
+
+app = FastAPI()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL, echo=True, future=True)
+
+@app.get("/")
+def root():
+    return {"message": "Servie LISTA is running"}
+
+#
+#@app.get("/items")
+#def get_items():
+#    with engine.connect() as conn:
+#        result = conn.execute(text("SELECT id, nombre, cantidad, tipo FROM item"))
+#        items = [dict(row._mapping) for row in result]
+#    return {"items": items}
